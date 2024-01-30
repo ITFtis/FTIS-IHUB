49f3412321dcd14a3587ceec77ee99f4e8187c82
brianlin12345 <brianlin12345@gamil.com>
2024-01-30 09:46:50 +0800

    履約 v3 天數比較

--- Scripts/prj/Home.js
+++ Scripts/prj/Home.js
@@ -60,11 +60,29 @@
                     content = '<ul>';
                     $.each(data.AlertPJs, function (index, value) {
 
-                        var msg = '<span>(' + (index + 1) + ')' + this.pjds1 + '</span>'
-                            + '<span class="ps-3">預定完成(' + this.date4 + ' ' + ')   ' + this.date4_diffday + '天</span>'
-                            + '<span class="ps-3">合約規範(' + this.date3 + ' ' + ')   ' + this.date3_diffday + '天</span>';
+                        var date3_desc = '';
+                        var date4_desc = '';
 
-                        content += '<li class="list-group">' + msg + '</li>';
+                        if (this.date3_diffday >= 0) {
+                            date3_desc = '剩' + (this.date3_diffday) + '天';
+                        }
+                        else {
+                            date3_desc = '<span class = "text-primary">' + '逾' + (this.date3_diffday * -1) + '天</span>';
+                        }
+
+                        if (this.date4_diffday >= 0) {
+                            date4_desc = '剩' + (this.date4_diffday) + '天';
+                        }
+                        else {
+                            date4_desc = '<span class = "text-primary">' + '逾' + (this.date4_diffday * -1) + '天</span>';
+                        }
+
+                        var msg = '<span>(' + (index + 1) + ')' + this.pjds1 + '</span>'
+                            + '<span class="ps-3">工作項目：' + this.pjds2 + '</span>'
+                            + '<span class="ps-3">預定完成(' + this.date4 + ' ' + ')   ' + date4_desc + '</span>'
+                            + '<span class="ps-3">合約規範(' + this.date3 + ' ' + ')   ' + date3_desc + '</span>';
+
+                        content += '<li class="list-group pb-3">' + msg + '</li>';
                     });
 
                     content += '</ul>';
